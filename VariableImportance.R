481b9afcc27155e0f81e668726741bf420ffb682
Nana Akwasi Abayie Boateng <nboateng@memphis.edu>
2018-08-19 17:44:49 -0400

    machine learning in r

---
+++ VariableImportance.R
@@ -0,0 +1,58 @@
+Variable_importance <- function(var, imp, colours = NA, limit = 15, model_name = NA, subtitle = NA,
+                             save = FALSE, file_name = "viz_importance.png", subdir = NA) {
+  
+  require(ggplot2)
+  require(gridExtra)
+  options(warn=-1)
+  
+  if (length(var) != length(imp)) {
+    message("The variables and importance values vectors should be the same length.")
+    stop(message(paste("Currently, there are",length(var),"variables and",length(imp),"importance values!")))
+  }
+  if (is.na(colours)) {
+    #colours <- "deepskyblue"
+    colours <- "#35B779FF"
+  }
+  out <- data.frame(var = var, imp = imp, Type = colours)
+  if (length(var) < limit) {
+    limit <- length(var)
+  }
+  
+  output <- out[1:limit,]
+  
+  p <- ggplot(output, 
+              aes(x = reorder(var, imp), y = imp * 100, 
+                  label = round(100 * imp, 1))) + 
+    geom_col(aes(fill = Type), width = 0.1) +
+    geom_point(aes(colour = Type), size = 6) + 
+    coord_flip() + xlab('') + theme_minimal() +
+    ylab('Importance') + 
+    geom_text(hjust = 0.5, size = 2, inherit.aes = TRUE, colour = "white") +
+    #geom_label(aes(label=round(Importance,0),vjust=0.2))+
+    
+    labs(title = paste0("Variables Importances. (", limit, " / ", length(var), " plotted)"))
+  
+  if (length(unique(output$Type)) == 1) {
+    p <- p + geom_col(fill = colours, width = 0.2) +
+      geom_point(colour = colours, size = 6) + 
+      guides(fill = FALSE, colour = FALSE) + 
+      geom_text(hjust = 0.5, size = 2, inherit.aes = TRUE, colour = "white")
+  }
+  if(!is.na(model_name)) {
+    p <- p + labs(caption = model_name)
+  }
+  if(!is.na(subtitle)) {
+    p <- p + labs(subtitle = subtitle)
+  }  
+  if(save == TRUE) {
+    if (!is.na(subdir)) {
+      dir.create(file.path(getwd(), subdir))
+      file_name <- paste(subdir, file_name, sep="/")
+    }
+    p <- p + ggsave(file_name, width=7, height=6)
+  }
+  
+  return(p)
+  
+}
+
